a646fbd247470e53f7fbea79e36580e2e3b5ef78
kingle1024 <teran1024@naver.com>
2024-05-11 10:21:47 +0900

    ✨ Feat: 필터 기능 적용

--- src/main/java/com/travel/api/repository/RegionRepository.java
+++ src/main/java/com/travel/api/repository/RegionRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.travel.api.vo.Region;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
     List<Region> findByRegionCdIn(List<String> regionCd);
+
+    @Query("SELECT r.regionCd, r.regionNm FROM Region r GROUP BY r.regionCd, r.regionNm")
+    List<Object[]> findAllGroupByRegionCdAndRegionNm();
 }
