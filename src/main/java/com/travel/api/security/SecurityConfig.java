0ca9b3557265196e3a48a0d21b38b3be978ab1f8
kingle1024 <teran1024@naver.com>
2024-06-30 09:45:55 +0900

    🚨 Fix: origin 추가(서버 포트)

--- src/main/java/com/travel/api/security/SecurityConfig.java
+++ src/main/java/com/travel/api/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:8100"); // Local
         config.addAllowedOrigin("http://localhost:8091"); // Server
+        config.addAllowedOrigin("http://datecourse.net"); // Server
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
