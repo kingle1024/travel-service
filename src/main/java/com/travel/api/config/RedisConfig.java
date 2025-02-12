b61b2b1496295a58fe8e04cabe38393a7f9d4f22
kingle1024 <teran1024@naver.com>
2025-02-12 23:07:29 +0900

    ✨ Feat: 레디스 dev, prd 소스 분기처리(단일 레디스, 클러스터링 레디스)

--- src/main/java/com/travel/api/config/RedisConfig.java
+++ src/main/java/com/travel/api/config/RedisConfig.java
@@ -1,9 +1,16 @@
 package com.travel.api.config;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -14,23 +21,49 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String host;
+    @Value("${spring.redis.password}")
+    private String password;
 
-    @Value("${spring.redis.port}")
-    private int port;
-
+    // 개발 환경 설정
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    @Profile("dev")
+    public RedisConnectionFactory redisStandaloneConnectionFactory (
+        @Value("${spring.redis.host}") String host,
+        @Value("${spring.redis.port}") int port) {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setPassword(password);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(2)) // 연결 타임아웃 설정
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+
+    // 운영 환경 설정
+    @Bean
+    @Profile("prod")
+    public RedisConnectionFactory redisClusterConnectionFactory (
+        @Value("${spring.redis.cluster.nodes}") String clusterNodes) {
+
+        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration(
+            Arrays.asList(clusterNodes.split(",")));
+        clusterConfig.setPassword(password); // 비밀번호 설정
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(2)) // 연결 타임아웃 설정
+                .build();
+
+        return new LettuceConnectionFactory(clusterConfig, clientConfig);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
